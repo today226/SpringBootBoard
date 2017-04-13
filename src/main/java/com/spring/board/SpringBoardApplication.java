effd4cd1ea4cef14daf1154814103e498793f7a8
today226 <today226@nate.com>
2017-04-14 04:03:46 +0900

    Initial commit

---
+++ src/main/java/com/spring/board/SpringBoardApplication.java
@@ -0,0 +1,12 @@
+package com.spring.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoardApplication.class, args);
+	}
+}
